f764a731bac2f530993b56d5c4a924ae15e64e1e
MamathaRY <mamathary@gmail.com>
2022-12-07 14:04:04 +0530

    Program 2

--- BasicJavaPrograms/src/sample/Integers.java
+++ BasicJavaPrograms/src/sample/Integers.java
@@ -2,10 +2,10 @@ package sample;
 
 public class Integers {
 	public static void main(String args[]) {
-		int c;
+		int a;
 
-		for (c = 1; c <= 10; c++) {
-			System.out.println(c);
+		for (a = 1; a <= 10; a++) {
+			System.out.println(a);
 		}
 
 	}
