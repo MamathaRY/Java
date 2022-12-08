3cfa727ae35307457e56adce35b1ced194fffb2c
MamathaRY <mamathary@gmail.com>
2022-12-08 15:35:14 +0530

    Program 10

---
+++ BasicJavaPrograms/src/sample/Alphabets.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Alphabets {
+
+	public static void main(String args[]) {
+		char ch;
+
+		for (ch = 'a'; ch <= 'z'; ch++) {
+			System.out.print(ch + "\n");
+		}
+
+	}
+}
