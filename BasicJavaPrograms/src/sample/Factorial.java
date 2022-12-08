1c37d2d30011d7a2036b38a55e68c2252d323e7a
MamathaRY <mamathary@gmail.com>
2022-12-08 11:40:56 +0530

    Program 8

--- BasicJavaPrograms/src/sample/Factorial.java
+++ BasicJavaPrograms/src/sample/Factorial.java
@@ -2,20 +2,17 @@ package sample;
 
 public class Factorial {
 
- int factorial(int n) {
+	static int factorial(int n) {
 		int fact = 1;
 		for (int i = 2; i <= n; i++) {
 			fact = fact * i;
 		}
 		return fact;
 	}
- 
+
 	public static void main(String args[]) {
 		int n = 5;
-		int m;
-		Factorial f = new Factorial();
-		m = f.factorial(n);
-		System.out.println("Factorial =" + m);
+		System.out.println("Factorial =" + factorial(n));
 
 	}
 }
