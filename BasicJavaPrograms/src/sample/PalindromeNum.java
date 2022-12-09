b71ffd52676cbe5601b96ba1d7c2b6a1e94ae30d
MamathaRY <mamathary@gmail.com>
2022-12-09 15:08:57 +0530

    Updated

---
+++ BasicJavaPrograms/src/sample/PalindromeNum.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class PalindromeNum {
+
+	public static void main(String[] args) {
+		int n = 454;
+		int temp = n;
+
+		int rev = 0; // reversed number
+		int rem; // remainder
+
+		while (n > 0) {
+
+			rem = n % 10;
+			rev = (rev * 10) + rem;
+			n = n / 10;
+		}
+		if (temp == rev)
+			System.out.println("palindrome number ");
+		else
+			System.out.println("not palindrome");
+	}
+
+}
