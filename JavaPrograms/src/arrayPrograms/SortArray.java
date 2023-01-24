15ceccb7c3ba1a32d542db3cd01c9fb61eea748a
Mamatha R <mamatha.r@tadigital.com>
2023-01-24 12:08:47 +0530

    Updated

--- JavaPrograms/src/arrayPrograms/SortArray.java
+++ JavaPrograms/src/arrayPrograms/SortArray.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class SortArray {
 	public static void main(String[] args) {
+		
 		int arr[] = { 10, 20, 15, 30, 1, 5, 12 };
 		System.out.println("Before :" + Arrays.toString(arr));
 		Arrays.sort(arr);
